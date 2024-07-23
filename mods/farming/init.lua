46eca51a483320112e3441f7c83e226d3be628e4
Heather <gitmail@emptystar.email>
2024-07-22 23:09:36 -0400

    farming: do not register cotton decoration

--- mods/farming/init.lua
+++ mods/farming/init.lua
@@ -71,25 +71,6 @@ farming.register_plant("farming:cotton", {
 	groups = {flammable = 4},
 })
 
-minetest.register_decoration({
-	name = "farming:cotton_wild",
-	deco_type = "simple",
-	place_on = {"default:dry_dirt_with_dry_grass"},
-	sidelen = 16,
-	noise_params = {
-		offset = -0.1,
-		scale = 0.1,
-		spread = {x = 50, y = 50, z = 50},
-		seed = 4242,
-		octaves = 3,
-		persist = 0.7
-	},
-	biomes = {"savanna"},
-	y_max = 31000,
-	y_min = 1,
-	decoration = "farming:cotton_wild",
-})
-
 minetest.register_craftitem("farming:string", {
 	description = S("String"),
 	inventory_image = "farming_string.png",
